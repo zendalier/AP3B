6010a9c17066ee12e8d15007ce50f2299714a08c
zendalier <144040468+zendalier@users.noreply.github.com>
2025-10-18 22:59:49 +0700

    fixed spaces

--- array.go
+++ array.go
@@ -1,20 +1,18 @@
-package main 
+package main
+
 import "fmt"
 
 func main() {
-    // Deklarasi array dengan ukuran tetap
-    var angka [5]int
-    angka[0] = 10
-    angka[1] = 20
-    angka[2] = 30
-    angka[3] = 40
-    angka[4] = 50
-    
-    fmt.Println("Array angka:", angka)
-    fmt.Println("Panjang array:", len(angka))
-    
-    // Deklarasi dan inisialisasi langsung
-    buah := [4]string{"Apel", "Jeruk", "Mangga", "Pisang"}
-    fmt.Println("\nArray buah:", buah)
-   
+	var angka [5]int
+	angka[0] = 10
+	angka[1] = 20
+	angka[2] = 30
+	angka[3] = 40
+	angka[4] = 50
+
+	fmt.Println("Array angka:", angka)
+	fmt.Println("Panjang array:", len(angka))
+
+	buah := [4]string{"Apel", "Jeruk", "Mangga", "Pisang"}
+	fmt.Println("\nArray buah:", buah)
 }
